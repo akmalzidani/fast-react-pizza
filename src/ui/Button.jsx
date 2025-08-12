197af36b37a224ffa9c35f43332fb0940900d405
akmalzidani <akmal20031003@gmail.com>
2025-08-12 07:45:37 +0700

    feat: enhance CreateOrder, Order, and OrderItem components with improved layout and styling; add Button and LinkButton components for better UI interaction

---
+++ src/ui/Button.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+function Button({ children, disabled, to, type = "primary" }) {
+  const base =
+    "inline-block  text=sm font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full hover:bg-yellow-300 text-stone-800 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed";
+
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    secondary:
+      "inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 border-2 border-stone-300 rounded-full hover:bg-stone-300 hover:text-stone-800 text-stone-400 focus:outline-none focus:ring focus:ring-stone-200 focus:bg-stone-300 focus:text-stone-800 focus:ring-offset-2 disabled:cursor-not-allowed px-4 py-2.5 md:px-6 md:py-3.5",
+  };
+
+  if (to) {
+    return (
+      <Link to={to} className={styles[type]}>
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <button disabled={disabled} className={styles[type]}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
