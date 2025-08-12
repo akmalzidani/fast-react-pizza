6bcdf14685307e2c97aa647acd2961b02e661cd1
akmalzidani <akmal20031003@gmail.com>
2025-08-12 07:45:19 +0700

    feat: enhance SearchOrder input placeholder and styling; improve layout in AppLayout and Home components

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,9 +16,10 @@ function SearchOrder() {
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        placeholder="SearchOrder"
+        placeholder="Search Order 🔍"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 text-sm transition-all duration-300 bg-yellow-100 rounded-full placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50"
       />
     </form>
   );
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,13 +8,15 @@ export default function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="layout">
+    <div className="grid  grid-rows-[auto_1fr_auto] h-screen">
       {isLoading && <Loader />}
       <Header />
 
-      <main>
-        <Outlet />
-      </main>
+      <div className="my-10 overflow-y-auto">
+        <main className="max-w-3xl mx-auto ">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
