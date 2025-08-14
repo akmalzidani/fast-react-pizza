36e9be212d1e983a405cafac0da5336534979ce9
akmalzidani <akmal20031003@gmail.com>
2025-08-14 09:16:27 +0700

    feat: update CartOverview component to display total quantity and price from Redux state

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -32,3 +32,9 @@ const cartSlice = createSlice({
 
 export const { addItem, deleteItem, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
+
+export const getTotalCartQuantity = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
+export const getTotalCartPrice = (state) =>
+  state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
